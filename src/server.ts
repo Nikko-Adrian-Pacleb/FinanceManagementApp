c86a3769307368f8c7b786a4f62346bf97dbec21
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2023-06-04 20:58:14 -0700

    eslint initialized

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import express from "express";
 
-const port = 5000;
-
 const app = express();
+const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Server Initialization");
 });
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
