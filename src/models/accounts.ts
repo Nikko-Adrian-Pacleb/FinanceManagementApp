4ab0a81cfcc2cbdebf4d0b05d7b9476bb0db0bea
NikkoPacleb <pacleb.nikko@gmail.com>
2023-06-19 23:08:50 -0700

    account removed redundant code block

--- src/models/accounts.ts
+++ src/models/accounts.ts
@@ -1,7 +1,5 @@
 import { InferSchemaType, Schema, model } from "mongoose";
 
-export type AccountDocument = InferSchemaType<typeof accountSchema>;
-
 const accountSchema = new Schema(
   {
     AccountId: {
