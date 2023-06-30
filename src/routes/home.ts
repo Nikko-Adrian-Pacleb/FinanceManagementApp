6d2a11cf84be23c0300e28323dbdcb8ed84a5acc
NikkoPacleb <pacleb.nikko@gmail.com>
2023-06-29 23:18:51 -0700

    IsAuth added back to home page. IsAuth test works after login

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -4,7 +4,7 @@ import { isAuth } from "../middleware/Authenticated";
 const router = express.Router();
 
 //-- Routes Start --//
-router.get("/", (req, res, next) => {
+router.get("/", isAuth, (req, res, next) => {
   res.render("home_page", {
     title: "Home",
   });
